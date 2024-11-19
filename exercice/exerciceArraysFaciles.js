b961b3c220343fdcc2078e0ef8f072d8e989db01
BS4002 <bakkousalma@gmail.com>
2024-11-19 16:03:49 +0100

    exo3 et exo4

--- exercice/exerciceArraysFaciles.js
+++ exercice/exerciceArraysFaciles.js
@@ -4,7 +4,24 @@ console.log("--------------------------------------------")
 console.log("EXERCICE 1:")
 let nombres=[1,2,3,4,5];
 console.log(nombres);
+console.log("--------------------------------------------")
 //exercice2
 console.log("EXERCICE 2:")
 let fruits=["apple","banana","cherry"];
 console.log(fruits[1]);
+console.log("--------------------------------------------")
+//exercice3
+console.log("EXERCICE 3:")
+let nbr=[10,20,30,40];
+console.log(nbr)
+nbr[2]= 50;
+console.log(nbr);
+console.log("--------------------------------------------")
+//exercice4
+console.log("EXERCICE 4:")
+let fruit=["mango","peach","pear"]
+console.log(fruit)
+fruit[3]="banana";
+console.log(fruit);
+
+
