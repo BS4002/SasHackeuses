e0c06ce362262d788057e55bfceaf6ccfa39078e
BS4002 <bakkousalma@gmail.com>
2024-11-20 12:05:53 +0100

    ex13

--- exercice/exerciceArraysMoyens.js
+++ exercice/exerciceArraysMoyens.js
@@ -12,3 +12,11 @@ let pairs=tableau2.filter(function(n){
     return n%2===0;
 });
 console.log(pairs);
+console.log("--------------------------------------------")
+//exercice13
+console.log("EXERCICE13:")
+let a=[1,2,3];
+let b=[4,5,6];
+let c=a.concat(b);
+console.log(c);
+
